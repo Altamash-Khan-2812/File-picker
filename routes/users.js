0d93180c32b41745d452e533c9353f3eb50c0b3b
Altamash-Khan-2812 <altamashkhn281@gmail.com>
2025-09-28 15:09:42 +0530

    configured multer storage to preserve file extensions

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const multer = require("multer");
 
-const upload = multer({ dest: "images" });
+const storageConfig = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "images");
+  },
+  filename: function (req, file, cb) {
+    cb(null,  Date.now() + "-" + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storageConfig });
 const router = express.Router();
 
 router.get("/", function (req, res) {
@@ -16,9 +25,9 @@ router.post("/profiles", upload.single("image"), function (req, res) {
   const uploadedImageFile = req.file;
   const userData = req.body;
 
-  console.log('uploadedImageFile', uploadedImageFile);
-  console.log('userData', userData);
-  res.redirect("/")
+  console.log("uploadedImageFile", uploadedImageFile);
+  console.log("userData", userData);
+  res.redirect("/");
 });
 
 module.exports = router;
